8880e4bfc4b1f935e340e40867e56eabf0769401
徐历敏 <xulimin@zuoyebang.com>
2022-02-20 16:59:53 +0800

    feat： ts-react loader

---
+++ ts/ts-react/webpack.react.js
@@ -0,0 +1,37 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+  entry:{
+    index: './src/ReactHell.tsx'
+  },
+  mode: "development",
+  module:{
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve:{
+    extensions:['.tsx','.ts','.js']
+  },
+  output :{
+    filename:"bundle.[name].js",
+    path: path.resolve(__dirname,'dist')
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname,'template.html')
+    })
+  ],
+  devServer:{
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port:3020
+  }
+
+}
